375b5181db155fcee07e99cce003ca513fe05e50
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-03-11 18:59:11 +0300

    dev: shopLayout

--- src/widgets/ShopLayout/ui/ShopLayout.tsx
+++ src/widgets/ShopLayout/ui/ShopLayout.tsx
@@ -1,8 +1,12 @@
-import { Outlet } from 'react-router-dom'
+import { RoutesName } from '@/app/providers/router'
+import { Link, Outlet } from 'react-router-dom'
 
 const ShopLayout = () => {
+    console.log('shopLa')
+
     return (
         <main>
+            <Link to={RoutesName.MAIN}>Главаня</Link>
             <Outlet />
         </main>
     )
