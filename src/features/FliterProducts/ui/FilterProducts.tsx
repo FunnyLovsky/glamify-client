f73fba1390cb8883d1e311fa8ee6c5bbcdd42248
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-03-21 23:29:32 +0300

    dev: filters Product

--- src/features/FliterProducts/ui/FilterProducts.tsx
+++ src/features/FliterProducts/ui/FilterProducts.tsx
@@ -1,9 +1,29 @@
 import styles from './FilterProducts.module.scss'
 import { useLocation } from 'react-router-dom'
 import ARROW from '@/shared/assets/elements/arrow.svg'
+import AppButton from '@/shared/ui/AppButton'
+import { useState } from 'react'
+
+interface Filters {
+    category: string
+    gender: string
+    style: string[]
+    size: string[]
+    color: string[]
+    price: string
+}
 
 const FilterProducts = () => {
+    const [filters, setFilters] = useState<Filters>({} as Filters)
     const { pathname, search } = useLocation()
+
+    const onChangeGender = (gender: 'Men' | 'Women') => {
+        if (gender == filters.gender) {
+            setFilters((prev) => ({ ...prev, gender: '' }))
+        } else {
+            setFilters((prev) => ({ ...prev, gender }))
+        }
+    }
 
     return (
         <div className={styles.filters}>
@@ -17,6 +37,24 @@ const FilterProducts = () => {
                         <ARROW />
                     </button>
                 </div>
+                <div className={styles.check}>
+                    <label htmlFor="men">Мужчинам</label>
+                    <input
+                        type="checkbox"
+                        checked={filters.gender == 'Men'}
+                        onChange={() => onChangeGender('Men')}
+                        id="men"
+                    />
+                </div>
+                <div className={styles.check}>
+                    <label htmlFor="women">Женщинам</label>
+                    <input
+                        type="checkbox"
+                        checked={filters.gender == 'Women'}
+                        onChange={() => onChangeGender('Women')}
+                        id="women"
+                    />
+                </div>
             </div>
             <div className={styles.filter}>
                 <div className={styles.head}>
@@ -26,6 +64,9 @@ const FilterProducts = () => {
                     </button>
                 </div>
             </div>
+            <AppButton variant="black" type="big">
+                Применить
+            </AppButton>
         </div>
     )
 }
